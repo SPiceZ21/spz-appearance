dd714ad3202baf311ce1ad941e864b9d14425774
Yuga Bharathi Jaisankar <yuga.bharathijai2106@gmail.com>
2026-05-19 16:04:21 +0530

    feat: update default male uniform appearance preset

--- config.lua
+++ config.lua
@@ -6,16 +6,20 @@ Config.DefaultOutfit = {
   [0] = {   -- Male uniform
     gender     = 0,
     components = {
-      [3]  = { 1,  0, 0 },   -- arms
-      [4]  = { 5,  0, 0 },   -- legs
-      [6]  = { 13, 0, 0 },   -- shoes
-      [11] = { 375, 0, 0 },  -- jacket
+      [1]  = { 0,   0, 0 },   -- head
+      [3]  = { 15,  0, 0 },   -- arms
+      [4]  = { 61,  0, 0 },   -- legs
+      [6]  = { 34,  0, 0 },   -- shoes
+      [7]  = { 0,   0, 0 },   -- accessories
+      [8]  = { 15,  0, 0 },   -- undershirt
+      [11] = { 55,  0, 0 },   -- torso / jacket
     },
     props = {
-      [0] = { 53, 0 },       -- hat/helmet
-      [1] = { 1, 0 },        -- glasses
-      [6] = { 1, 0 },        -- watch
-      [7] = { 1, 0 },        -- bracelet
+      [0] = { -1, 0 },   -- hat
+      [1] = { -1, 0 },   -- glasses
+      [2] = { -1, 0 },   -- ear
+      [6] = { -1, 0 },   -- watch
+      [7] = { -1, 0 },   -- bracelet
     }
   },
 
