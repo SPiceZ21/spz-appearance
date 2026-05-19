6843a5c5c435e2d07ac11c6d5e296d9c49d446ea
Yuga Bharathi Jaisankar <yuga.bharathijai2106@gmail.com>
2026-05-19 16:05:09 +0530

    feat: synchronize default female uniform with default male uniform preset

--- config.lua
+++ config.lua
@@ -26,15 +26,20 @@ Config.DefaultOutfit = {
   [1] = {   -- Female uniform
     gender     = 1,
     components = {
-      [1]  = { 169, 0, 0 },  -- mask
-      [3]  = { 14, 0, 0 },   -- arms
-      [4]  = { 44, 0, 0 },   -- legs
-      [6]  = { 68, 0, 0 },   -- shoes
-      [8]  = { 14, 0, 0 },   -- undershirt
-      [11] = { 392, 0, 0 },  -- jacket
+      [1]  = { 0,   0, 0 },   -- head
+      [3]  = { 15,  0, 0 },   -- arms
+      [4]  = { 61,  0, 0 },   -- legs
+      [6]  = { 34,  0, 0 },   -- shoes
+      [7]  = { 0,   0, 0 },   -- accessories
+      [8]  = { 15,  0, 0 },   -- undershirt
+      [11] = { 55,  0, 0 },   -- torso / jacket
     },
     props = {
-      [0] = { 50, 0 },       -- hat/helmet
+      [0] = { -1, 0 },   -- hat
+      [1] = { -1, 0 },   -- glasses
+      [2] = { -1, 0 },   -- ear
+      [6] = { -1, 0 },   -- watch
+      [7] = { -1, 0 },   -- bracelet
     }
   }
 
